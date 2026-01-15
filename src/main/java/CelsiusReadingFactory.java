d5db805abdc2a60a5c8195096e6dfc6a20694321
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2026-01-15 12:51:18 +0100

    SensorReadingFactoryTests grün

--- src/main/java/CelsiusReadingFactory.java
+++ src/main/java/CelsiusReadingFactory.java
@@ -1,6 +1,17 @@
 public class CelsiusReadingFactory implements SensorReadingFactory{
     @Override
     public SensorReading createFromString(String s) {
-        return null;
+        //"175;60;31.5"
+        String[] parts = s.split(";");
+        
+        if(parts.length != 3){
+            throw new WeatherException("Reading has invalid line length!");
+        }
+        
+        int x = Integer.parseInt(parts[0].trim());
+        int y = Integer.parseInt(parts[1].trim());
+        double temperatureCelsius = Double.parseDouble(parts[2].trim());
+        
+        return new SensorReading(x, y, temperatureCelsius);
     }
 }
--- src/main/java/FahrenheitReadingFactory.java
+++ src/main/java/FahrenheitReadingFactory.java
@@ -1,6 +1,20 @@
 public class FahrenheitReadingFactory  implements SensorReadingFactory{
     @Override
     public SensorReading createFromString(String s) {
-        return null;
+        // "90;120;-13.0" ->
+        String[] parts = s.split(";");
+
+        if(parts.length != 3){
+            throw new WeatherException("Reading has invalid line length!");
+        }
+
+        int x = Integer.parseInt(parts[0].trim());
+        int y = Integer.parseInt(parts[1].trim());
+        //TC =
+        //5/9
+        //∗ (TF − 32)
+        double temperatureCelsius = Math.round(((double) 5 /9)*(Double.parseDouble(parts[2].trim()) - 32));
+
+        return new SensorReading(x, y, temperatureCelsius);
     }
 }
